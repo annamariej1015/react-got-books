1490c25bbfd90fd4bafa2413e3955329fa989580
Anna <annamariej1015@gmail.com>
2021-05-06 22:02:29 -0700

    #1: base structure added to files

---
+++ src/components/SearchFilterComponent.js
@@ -0,0 +1,11 @@
+const SearchFilterComponent = () => {
+    return(
+        <div className="row">
+            <div className="col">
+                <h2>Search Filter</h2>
+            </div>
+        </div>
+    );
+};
+
+export default SearchFilterComponent;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return(
+        <div className="row">
+            <div className="col">
+                <h2>Home page</h2>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
